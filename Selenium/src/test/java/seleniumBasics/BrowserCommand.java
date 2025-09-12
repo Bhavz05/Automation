c52aa02c414d9884f719cab082c4a7e9188eab3a
user <user@DESKTOP-FMLQ74F>
2025-09-12 18:13:50 +0530

    browsercommand

---
+++ Selenium/src/test/java/seleniumBasics/BrowserCommand.java
@@ -0,0 +1,29 @@
+package seleniumBasics;
+
+public class BrowserCommand extends Base{
+public void verifyBrowser()
+{
+	String Title=driver.getTitle();
+	System.out.println(Title);
+	String url=driver.getCurrentUrl();
+	System.out.println(url);
+	String source=driver.getPageSource();
+	System.out.println(source);
+}
+public void VerifyNavigation()
+{
+	driver.navigate().to("https://www.amazon.com/");
+	driver.navigate().back();
+	driver.navigate().forward();
+	driver.navigate().refresh();
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BrowserCommand browser=new BrowserCommand();
+browser.InitializeBrowser();
+browser.verifyBrowser();
+browser.VerifyNavigation();
+	}
+
+}
